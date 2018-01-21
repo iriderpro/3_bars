2311deb1e597cac2c214e2dc55900abcdbf5a748
iriderpro <irider.pro@gmail.com>
2018-01-22 00:17:51 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -10,21 +10,34 @@ def load_data(filepath):
     return python_obj
 
 
-def find_min_bar(dict):
-    min_bar = min(dict['features'],
+def find_min_bar(dict_bars):
+    min_bar = min(dict_bars['features'],
                   key=lambda i: i['properties']['Attributes']['SeatsCount'])
     return min_bar
 
 
-def find_max_bar(dict):
-    max_bar = max(dict['features'],
+def find_max_bar(dict_bars):
+    max_bar = max(dict_bars['features'],
                   key=lambda i: i['properties']['Attributes']['SeatsCount'])
     return max_bar
 
 
-def find_nearest_bar(x1, y1, x2, y2, name_bar):
-    distance = hypot(x2 - x1, y2 - y1)
-    return distance, name_bar
+def find_nearest_bar(py_obj_bars):
+    while True:
+        try:
+            your_dolgota = float(input('Введите долготу : '))
+            your_shirota = float(input('Введите широту  : '))
+            break
+        except ValueError:
+            print('Неверный формат. Попробуй еще раз...')
+
+    list_bars = []
+    for bar in py_obj_bars['features']:
+        distance = hypot(your_dolgota - bar['geometry']['coordinates'][0],
+                         your_shirota - bar['geometry']['coordinates'][1])
+        list_bars.append((distance, bar['properties']['Attributes']['Name']))
+    near_bar = min(list_bars)
+    return near_bar
 
 
 if __name__ == '__main__':
@@ -33,31 +46,14 @@ if __name__ == '__main__':
             py_obj_bars = load_data(sys.argv[1])
             min_bar = find_min_bar(py_obj_bars)
             max_bar = find_max_bar(py_obj_bars)
+
             print('Самый маленький бар',
                   min_bar['properties']['Attributes']['Name'])
             print('Самый большой бар',
                   max_bar['properties']['Attributes']['Name'])
 
-            while True:
-                try:
-                    your_dolgota = float(input('Введите долготу : '))
-                    your_shirota = float(input('Введите широту  : '))
-                    break
-                except ValueError:
-                    print('Неверный формат. Попробуй еще раз...')
-                except Exception:
-                    print('Что то не так...')
-
-            list_bars = []
-            for bar in py_obj_bars['features']:
-                list_bars.append(
-                    find_nearest_bar(bar['geometry']['coordinates'][0],
-                                     bar['geometry']['coordinates'][1],
-                                     your_dolgota,
-                                     your_shirota,
-                                     bar['properties']['Attributes']['Name']))
-            min(list_bars)
-            print('Ближайший бар', min(list_bars)[1])
+            nearest_bar = find_nearest_bar(py_obj_bars)
+            print('Ближайший бар', nearest_bar[1])
         else:
             print('файла не существует')
     else:
